1b6856ea9fb0f4f2c2d7c6aa6b62ac2717b0ffd8
RaduRS <48391644+RaduRS@users.noreply.github.com>
2024-12-26 19:55:07 +0000

    change base url

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://presswisp-git-main-radurs-projects-d4653025.vercel.app"
+    : process.env.NEXT_PUBLIC_BASE_URL;
+
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/graphql`,
+  uri: `${baseUrl}/api/graphql`,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
