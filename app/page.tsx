0915e442a705d871966d078cc7c61099b69db025
RaduRS <48391644+RaduRS@users.noreply.github.com>
2024-12-27 09:25:10 +0000

    change8

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,48 @@
-import Cards from "@/components/CardComponent/Cards";
-import client from "@/lib/apolloClient";
-import { GET_ARTICLES, GET_HEADLINE_ARTICLE } from "./api/graphql/queries";
-import { Metadata } from "next";
-import { ArticleBase } from "@/types/types";
-import Headline from "@/components/Headline";
+// import Cards from "@/components/CardComponent/Cards";
+// import client from "@/lib/apolloClient";
+// import { GET_ARTICLES, GET_HEADLINE_ARTICLE } from "./api/graphql/queries";
+// import { Metadata } from "next";
+// import { ArticleBase } from "@/types/types";
+// import Headline from "@/components/Headline";
 
 export const dynamic = "force-static";
 export const revalidate = false;
-export async function generateMetadata(): Promise<Metadata> {
-  const { data } = await client.query({
-    query: GET_ARTICLES,
-    fetchPolicy: "network-only",
-  });
+// export async function generateMetadata(): Promise<Metadata> {
+//   const { data } = await client.query({
+//     query: GET_ARTICLES,
+//     fetchPolicy: "network-only",
+//   });
 
-  const articles = data?.getArticles || [];
+//   const articles = data?.getArticles || [];
 
-  const topArticle = articles[0];
-  const articleMetaTitles = articles
-    .map((article: ArticleBase) => article.metaTitle)
-    .slice(0, 3);
+//   const topArticle = articles[0];
+//   const articleMetaTitles = articles
+//     .map((article: ArticleBase) => article.metaTitle)
+//     .slice(0, 3);
 
-  return {
-    title: "PressWisp - Latest News",
-    description: `Explore the latest news and insights on PressWisp. Stay informed with curated articles on trending topics from trusted sources like: ${articleMetaTitles.join(
-      ", "
-    )}.`,
-    openGraph: {
-      title: "PressWisp - Latest News",
-      description: topArticle
-        ? `PressWisp delivers top news and insights. Check out our featured article: "${topArticle.metaTitle}".`
-        : "PressWisp delivers top news and insights, curated to keep you informed and ahead of the curve.",
-      type: "website",
-      url: "https://presswisp.com/",
-      images: [
-        {
-          url: topArticle?.imageSrc || "https://presswisp.com/default-og.png",
-          width: 1200,
-          height: 630,
-          alt: topArticle?.metaTitle || "PressWisp Homepage",
-        },
-      ],
-    },
-  };
-}
+//   return {
+//     title: "PressWisp - Latest News",
+//     description: `Explore the latest news and insights on PressWisp. Stay informed with curated articles on trending topics from trusted sources like: ${articleMetaTitles.join(
+//       ", "
+//     )}.`,
+//     openGraph: {
+//       title: "PressWisp - Latest News",
+//       description: topArticle
+//         ? `PressWisp delivers top news and insights. Check out our featured article: "${topArticle.metaTitle}".`
+//         : "PressWisp delivers top news and insights, curated to keep you informed and ahead of the curve.",
+//       type: "website",
+//       url: "https://presswisp.com/",
+//       images: [
+//         {
+//           url: topArticle?.imageSrc || "https://presswisp.com/default-og.png",
+//           width: 1200,
+//           height: 630,
+//           alt: topArticle?.metaTitle || "PressWisp Homepage",
+//         },
+//       ],
+//     },
+//   };
+// }
 
 export default async function Home() {
   // const { data: headlineData } = await client.query({
@@ -63,7 +63,7 @@ export default async function Home() {
     <div className="flex flex-col items-center w-full max-w-[1200px] mx-auto px-8 gap-16 sm:py-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">Welcome to the Press Wisp</h1>
       <section className="w-full max-w-[1024px] gap-16 flex flex-col">
-        {headlineArticle && (
+        {/* {headlineArticle && (
           <Headline
             title={headlineArticle.title}
             description={headlineArticle.description}
@@ -73,7 +73,7 @@ export default async function Home() {
             date={headlineArticle.date}
           />
         )}
-        <Cards articles={articles} />
+        <Cards articles={articles} /> */}
       </section>
       <footer className="text-gray-500">© 2024 Your Website</footer>
     </div>
